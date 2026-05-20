256f828518e64a78cb69e9aa0f3e5945fa5a5e0a
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-05-20 08:12:24 +0800

    Update 14b-2-modules-node.js

--- 14b-2-modules-node.js
+++ 14b-2-modules-node.js
@@ -1,5 +1,10 @@
-// /14b-2-modules-node.js
+// 14b-2-modules-node.js
 
-const math = require(".//14b-1-modules-node");
+/**
+ * Import the custom math module. 
+ * Resolves to the cached export object containing the `add` method.
+ */
+const math = require("./14b-1-modules-node");
 
-console.log(math.add(5)); // 5
+// Executes the add function, updating the module's internal state to 5.
+console.log(math.add(5)); // Output: 5
